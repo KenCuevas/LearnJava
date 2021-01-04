d8f1e1accd5d6d7e2f08f35e9dbb6e5b257919c7
KenCuevas <20182146@unapec.edu.do>
2021-01-04 00:26:42 -0400

    Actualizacion, mejora del bublesort ahora fue anadido como un metodo y puede ser utilizado para ordenar cualquier array.

--- src/Proyectos/BubbleSort.java
+++ src/Proyectos/BubbleSort.java
@@ -1,12 +1,37 @@
 package Proyectos;
 
 public class BubbleSort {
+    public static void bubblesort(Object [] arreglo){
+        int totales = arreglo.length;
+        int contador = 0;
+        for (int i = 0; i < totales - 1; i++ ){
+
+            for (int j = 0; j < totales -1 -i; j++){
+                if( ( (Comparable) arreglo[j+1]).compareTo(arreglo[j]) < 0 ){ //if( ((Integer)numero[j+1]).compareTo(numero[j])<0) opcion sin pasar a Integer
+                    Object aux = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = aux;
+                }
+                contador++;
+            }
+        }
+        System.out.println("Contador = " + contador);
+    }
     public static void main(String[] args) {
         String [] Productos = {"Television", "Nevera", "Microondas", "Laptop", "Xbox One S", "Playstation 4"};
-
+        Integer[] numero = {100,350,1,8,7,600,15,4,160,2,60,3,89,74,52,5,99,836,601,844,6,9};
+            bubblesort(Productos);
+            bubblesort(numero);
+        for (String producto : Productos) {
+            System.out.println(producto);
+        }
+        for (Integer integer : numero) {
+            System.out.println(integer);
+        }
+        /*
         //Utilizamos el metodo length para saber la longitud del array
         int cadenaTotal = Productos.length;
-        int contador = 0;
+
         //Necesitamos dos ciclos for, ya que el primero realiza el recorrido de cada elemento y el segundo compara cada elemento con cada uno.
         for (int i = 0; i < cadenaTotal;i++){
             for (int j = 0; j < cadenaTotal; j++){
@@ -19,6 +44,6 @@ public class BubbleSort {
                 contador++;
             }
         }
-        System.out.println("El algoritmo ha contado un total de: " + contador);
+        */
     }
 }
